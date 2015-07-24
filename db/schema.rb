757afc31d281d96e9f8c71b48fce79c6c747a952
Oskar Plichta <osi22@o2.pl>
2015-07-24 15:26:20 +0200

    Created user_name field in devise user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150723134903) do
+ActiveRecord::Schema.define(version: 20150724124642) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "caption"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150723134903) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "user_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
