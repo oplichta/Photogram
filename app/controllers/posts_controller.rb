97150bf13d914fae83cd05439ea0d0d91ac18051
Oskar Plichta <osi22@o2.pl>
2015-07-24 17:18:05 +0200

    Added comment model. Added associations between user, post and comment.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  
+  before_action :owned_post, only: [:edit, :update, :destroy]
   def index
     @posts = Post.all
   end
@@ -10,14 +10,15 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    @post = Post.create(post_params)
+    @post = current_user.posts.build(post_params)
+
     if @post.save
       flash[:success] = "Your post has been created."
-      redirect_to @post
+      redirect_to posts_path
     else
       flash[:alert] = "Halt, you fiend! You need an image to post here!"
       render :new
@@ -54,5 +55,12 @@ class PostsController < ApplicationController
 
   def set_post
     @post = Post.find(params[:id])
+  end
+
+  def owned_post
+    unless current_user == @post.user
+      flash[:alert] = "That post doesn't belong to you!"
+      redirect_to root_path
+    end
   end
 end
