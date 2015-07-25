2993979ed09cfea9e4e0827d6c1dcd8e751218a3
Oskar Plichta <osi22@o2.pl>
2015-07-25 13:38:43 +0200

    Added partial for comments. Improved styles in application.scss.

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,38 @@
+class CommentsController < ApplicationController
+  before_action :set_post
+
+  def create
+    @comment = @post.comments.build(comment_params)
+    @comment.user_id = current_user.id
+
+    if @comment.save
+      flash[:success] = "Your comment has been created."
+      redirect_to :back
+    else
+      flash[:alert] = "Something gone wrong. Check your comment form."
+      render root_path
+    end
+  end
+
+  def destroy
+    @comment = @post.comments.find(params[:id])
+
+    if @comment.destroy
+      flash[:success] = 'Comment was successfully destroyed.'
+      redirect_to root_path
+    else
+      flash[:alert] = "Something gone wrong... try again."
+      redirect_to root_path
+    end
+  end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
+
+    def set_post
+      @post = Post.find(params[:post_id])
+    end
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :owned_post, only: [:edit, :update, :destroy]
+  
   def index
     @posts = Post.all
   end
