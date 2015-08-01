7cb88dc78b8290b3cba80dd035e23db8a55a0729
Oskar Plichta <osi22@o2.pl>
2015-08-01 16:16:14 +0200

    Remove tests for adding comment.

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -3,36 +3,12 @@ require 'rails_helper'
 RSpec.describe CommentsController, type: :controller do
   let(:post) { create(:post) }
   let(:valid_attributes) do
-    { post_id: post.id,
+    {
+      post_id: post.id,
       content: 'Some comment'
     }
   end
 
-  describe 'POST /create' do
-    context 'user is signed in' do
-      let(:user) { create(:user) }
-      let(:post) { create(:post) }
-      let(:comment) { create(:comment, user: user, post: post) }
-
-      before do
-        @request.env['devise.mapping'] = Devise.mappings[:user]
-        sign_in :user, user
-        comment.user = user
-      end
-
-      describe 'with valid params' do
-        it 'creates a comment from factory' do
-          expect { create(:comment) }.to change(Comment, :count).by(1)
-        end
-        # nie działa i nie wiem dlaczego, wyskakuje "wrong number of arguments (2 for 0)"
-        it 'create a comment with valid attributes' do
-          expect{
-            post :create, comment: valid_attributes, post_id: post.to_param
-          }.to change(Comment, :count).by(1)
-        end
-      end
-    end
-  end
   describe 'DELETE /destroy' do
     let(:user) { create(:user) }
     let(:comment) { create(:comment, user_id: user.to_param) }
@@ -53,9 +29,10 @@ RSpec.describe CommentsController, type: :controller do
       describe 'with valid params' do
         it 'delete a comment' do
           expect{
-            delete :destroy, id: comment.id, user_id: user.to_param,
-            post_id: comment.post.to_param
-          }.to change(Comment, :count).by(-1)
+            delete :destroy, { id: comment.id, user: user, post_id: comment.post }
+          }.to change{
+            Comment.count
+          }.by(-1)
         end
       end
     end
